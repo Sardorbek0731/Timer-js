93187a3f19855dfb725405d92fae866fda087a4f
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-08-14 23:58:47 +0500

    I improved

--- setTimout & setInterval.js
+++ setTimout & setInterval.js
@@ -30,20 +30,31 @@
 const timer = document.querySelector("#timer");
 const timerBtn = document.querySelector("#timer_btn");
 const timerStopBtn = document.querySelector("#timerStop_btn");
+const by = document.querySelector('small')
+const href = document.querySelector('a')
 
 timer.textContent = 0;
 
 timerBtn.addEventListener("click", () => {
+
   let setNumber = setInterval(() => {
     timer.textContent++;
   }, 1000);
   timer.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
   timer.style.backdropFilter = "blur(5px)";
-  timer.style.padding = "15px 30px";
+  timer.style.padding = "15px 40px";
 
   timerStopBtn.addEventListener("click", () => {
     setTimeout(() => {
       clearInterval(setNumber);
     });
   });
+
 });
+
+by.style.position = "absolute";
+by.style.color = "black";
+by.style.bottom = "30px";
+by.style.fontSize = "20px";
+href.style.textDecoration = "none";
+href.style.color = "black";
